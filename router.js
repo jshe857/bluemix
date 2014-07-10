be6c7a29e0032ccd83ccb2aff9fb89e8525ac05d
Jeff Shen <jeshen@au1.ibm.com>
2014-07-10 16:24:50 +1000

    push latest changes

--- router.js
+++ router.js
@@ -1,11 +1,14 @@
 var app= require('express').Router();
 var user = require('./lib/user');
 var event = require('./lib/event');
-
+//user model operations
 app.post('/login', user.login);
 app.post('/register',user.register);
 app.post('/updateUser',user.update);
+
+//event model operations
 app.post('/createEvent',event.create);
 app.post('/getUserEvent',event.getUserEvent);
 app.post('/updateEvent',event.update);
+app.post('/removeEvent',event.remove)
 module.exports = app;
